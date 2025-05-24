578ff87e76d93a13d6a91fa4b1ac88d3412709a7
Anby <155241517+AnbyFox@users.noreply.github.com>
2025-05-24 13:30:45 +1000

    lab14 - opt fibonacci

--- C/Norm/labE/main.c
+++ C/Norm/labE/main.c
@@ -10,9 +10,17 @@ int fibonacci(int n) {
 		return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+int fibonacci_opt(int n, int prev2, int prev1) {
+    if (n == 0) return prev2;
+    if (n == 1) return prev1;
+    
+    return fibonacci_opt(n - 1, prev1, prev2 + prev1);
+}
+
 int sum_fib(jmp_buf buffer, int n) {
     int sum = 0;
-    for(int i = 0; i <= n; i++) sum += fibonacci(i);
+    //for(int i = 0; i <= n; i++) sum += fibonacci(i);
+    sum = fibonacci_opt(n, 0, 1);
 	
 	longjmp(buffer, sum);
 }
